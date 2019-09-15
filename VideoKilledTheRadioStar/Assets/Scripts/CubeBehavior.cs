1b9ba531d6857bcd3e52fe8f962696fdc73311ca
Ren H <kerensahughes@gmail.com>
2019-09-15 15:24:59 -0400

    StopDestroying
    If you press space (change channel) the cubes stop deleting because the timer went up. Temporary fix until can get help to increase cubes

--- VideoKilledTheRadioStar/Assets/Scripts/CubeBehavior.cs
+++ VideoKilledTheRadioStar/Assets/Scripts/CubeBehavior.cs
@@ -2,96 +2,111 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CubeBehavior : MonoBehaviour {
+public class CubeBehavior : MonoBehaviour
+{
 
 	//public GameObject []  ScreenCube;
 	//public GameObject[] cubes; 
 
 	public GameObject CubePrefab;
 
-	 GameObject [] Cubeclones = new GameObject[20]; 
+	GameObject[] Cubeclones = new GameObject[20];
+
 	//public GameObject[] cubes;
 	private int numofCubes = 10;
 
 
 	public int CountDownTime;
-	public int RevealTime;
+	public int RevealTime = 5;
 
 	private float timer;
 	private int cloneIndex;
-	
-	
+
+
 
 	// Use this for initialization
-	void Start () {
-		//cubes = new GameObject[numofCubes];
-		timer = 1;
+	void Start()
+	{
 
-		//StartCoroutine(Timer()); 
+
+		
 
 		for (int i = 0; i < 20; i++)
 		{
-			Cubeclones [i] = Instantiate(CubePrefab, new Vector3((transform.position.x*i*-0.15f), transform.position.y,0f ), Quaternion.identity) as GameObject;
+			Cubeclones[i] = Instantiate(CubePrefab,
+				new Vector3((transform.position.x * i * -0.15f), transform.position.y, 0f),
+				Quaternion.identity) as GameObject;
 			//cubes [i].SetActive(true);
 			//CubePrefab.SetActive(true);
 		}
+
 		print((Cubeclones.Length));
 		cloneIndex = Cubeclones.Length;
 
-		//cubes [i].SetActive(true);
-		//for (int i = 0; i < numofCubes; i++)
-		//{
-		//cubes [i] = Instantiate(ScreenCube[i]) as GameObject;
-
-		/*	if (i > 0)
-			{
-				cubes[i].transform.parent = cubes[i - 1].transform; 
-			}
-			
-			cubes[i].SetActive(true);*/
-		//}
-
-
-
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
+
+	// Update is called once per frame
+	void Update()
+	{
+			
 		if (timer > 0)
 		{
+			//print(timer);
 			timer -= Time.deltaTime;
+			
+
+			//else if (Input.GetKeyUp(KeyCode.Space))
+			//{
+			//	timer = 1; 
+			//}
+
 		}
 
 		else
 		{
-			Destroy((Cubeclones[cloneIndex -1 ]));
+			Destroy((Cubeclones[cloneIndex - 1]));
 			cloneIndex--;
 			timer = 1;
 		}
-
 		
-			/*if (CountDownTime > 1f)
-			{
-				for (int i = Cubeclones.Length; i > 0; i--)
-				{
-					
-				}
-					
-					
-				
-			}*/ 
-		
-		
-	}
-
-	IEnumerator Timer()
-	{
-		while (true)
+		if (Input.GetKeyDown(KeyCode.Space) && cloneIndex != 0)
 		{
-			yield return new WaitForSeconds(1);
-			CountDownTime++; 
-
+			//cloneIndex += 1; 
+			//print(cloneIndex);
+			timer = 5;
+			/*for (int i = 0; i < 1; i++)
+			{
+				Cubeclones [i] = Instantiate(CubePrefab, new Vector3((transform.position.x*i*-0.15f),
+					transform.position.y,0f ), Quaternion.identity) as GameObject;
+				//cubes [i].SetActive(true);
+			}*/
 		}
+
+
+		/*if (Input.GetKey(KeyCode.Space) && cloneIndex != 0)
+		{
+			cloneIndex += 1; 
+			
+			for (int i = 0; i < 1; i++)
+			{
+				Cubeclones [i] = Instantiate(CubePrefab, new Vector3((transform.position.x*i*-0.15f),
+					transform.position.y,0f ), Quaternion.identity) as GameObject;
+				//cubes [i].SetActive(true);
+				//CubePrefab.SetActive(true);
+			}
+		}
+
+		else
+		{
+			timer = 1; 
+		}*/
+		
+		
+		
 	}
+
+	
 }
+
+
